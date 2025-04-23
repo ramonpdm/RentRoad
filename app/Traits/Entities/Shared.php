db5470fab77a09c4584d87cedd10ae9b80eaa6bc
Ramón Perdomo <inoelperdomo@gmail.com>
2025-04-23 19:16:01 -0400

    fix: shared entities props

--- app/Traits/Entities/Shared.php
+++ app/Traits/Entities/Shared.php
@@ -6,9 +6,24 @@ use Doctrine\ORM\Mapping as ORM;
 
 trait Shared
 {
+    public function __construct(array $data = [])
+    {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->$key = $value;
+            }
+        }
+    }
+
     #[ORM\Column(type: 'boolean', options: ['default' => true])]
     public bool $activo = true;
 
     #[ORM\Column(type: 'datetime', options: ['default' => 'CURRENT_TIMESTAMP'])]
     public \DateTime $fecha_creacion;
+
+    #[ORM\PrePersist]
+    public function setFechaCreacion(): void
+    {
+        $this->fecha_creacion = new \DateTime();
+    }
 }
