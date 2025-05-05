58af6411c319e6447702dc2ed8edba870be6fbff
ramonpdm <inoelperdomo@gmail.com>
2025-05-05 14:58:00 -0400

    refactor: use bootstrap cdn instead

--- app/Views/inc/header.php
+++ app/Views/inc/header.php
@@ -11,9 +11,9 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
         <link rel="stylesheet" href="/public/fonts/icomoon/style.css">
-        <link rel="stylesheet" href="/public/css/bootstrap/bootstrap.min.css">
-        <link rel="stylesheet" href="/public/css/bootstrap/bootstrap-datepicker.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
         <link rel="stylesheet" href="/public/css/style.css">
+        <link rel="stylesheet" href="/public/css/bootstrap.css">
         <link rel="stylesheet" href="/public/css/utils.css">
     </head>
 
