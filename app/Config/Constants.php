16c9e9297267eb4aefc3fd40d6eb2a7ab9b6a52c
ramonpdm <inoelperdomo@gmail.com>
2025-05-05 15:50:54 -0400

    refactor: clean constant

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -15,20 +15,3 @@ define('DB_USER', $_ENV['DB_USER'] ?? '');
 define('DB_PASS', $_ENV['DB_PASS'] ?? '');
 define('DB_NAME', $_ENV['DB_NAME'] ?? '');
 define('DB_PORT', $_ENV['DB_PORT'] ?? '');
-
-/**
- * Time Constants
- */
-const APP_TIMEZONE = 'America/New_York';
-
-/**
- * ROLES
- */
-define('ROL_ADMIN', 1);
-define('ROL_EMPLEADO', 2);
-define('ROL_CLIENTE', 3);   
-
-/**
- * Configuracion de seccion
- */
-define('SESSION_USER_KEY', 'rentroad_user');
